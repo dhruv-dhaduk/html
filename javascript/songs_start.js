e752c1aca88f5fc105f14882bf935fad18c7385c
Dhruv Dhaduk <dhadukd44@gmail.com>
2023-07-30 17:43:50 +0530

    Changed the thumbnail quality

--- javascript/songs_start.js
+++ javascript/songs_start.js
@@ -3,7 +3,7 @@ function get_yt_thumbnail_link(link)
     const index = link.lastIndexOf('/');
     const videoID = link.substring(index + 1);
 
-    const thumbnail = "https://img.youtube.com/vi/" + videoID + "/hqdefault.jpg";
+    const thumbnail = "https://img.youtube.com/vi/" + videoID + "/mqdefault.jpg";
     
     return thumbnail;
 }
