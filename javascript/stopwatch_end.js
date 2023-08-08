d7feefb5800968e2afa79355993e6a600fa8c69d
Dhruv Dhaduk <dhadukd44@gmail.com>
2023-08-08 18:31:06 +0530

    Added the button listeners in stopwatch page

--- javascript/stopwatch_end.js
+++ javascript/stopwatch_end.js
@@ -1,12 +1,19 @@
 const minutes = document.getElementById("minutes");
 const seconds = document.getElementById("seconds");
 const mills = document.getElementById("mills");
+const startstop = document.getElementById("startstop");
+const reset = document.getElementById("reset");
+
+startstop.addEventListener("click", start_stopwatch);
+reset.addEventListener("click", reset_stopwatch);
 
 var mills_value = 0;
 var seconds_value = 0;
 var minutes_value = 0;
 
-var starting_time_mills = Date.now();
+var starting_time_mills;
+
+var is_running = false;
 
 function add_timeframe() 
 {
@@ -36,5 +43,50 @@ function add_timeframe()
 }
 
 var interval_id;
-starting_time_mills = Date.now();
-interval_id = setInterval(add_timeframe, 10);
+// starting_time_mills = Date.now();
+// interval_id = setInterval(add_timeframe, 10);
+
+function start_stopwatch()
+{
+    if (is_running == true)
+    {
+        return;
+    }
+
+    clearInterval(interval_id);
+
+    const current_time = Date.now();
+    const mills_on_watch = parseInt(mills.innerHTML) * 10 + parseInt(seconds.innerHTML) * 1000 + parseInt(minutes.innerHTML) * 60000;
+    starting_time_mills = current_time - mills_on_watch;
+
+    interval_id = setInterval(add_timeframe, 10);
+    
+    startstop.innerHTML = "STOP";
+    is_running = true;
+    startstop.removeEventListener("click", start_stopwatch);;
+    startstop.addEventListener("click", stop_stopwatch);
+}
+
+function stop_stopwatch()
+{
+    if (is_running == false)
+    {
+        return;
+    }
+
+    clearInterval(interval_id);
+
+    startstop.innerHTML = "START";
+    is_running = false;
+    startstop.removeEventListener("click", stop_stopwatch);
+    startstop.addEventListener("click", start_stopwatch);
+}
+
+function reset_stopwatch()
+{
+    mills.innerHTML = "00";
+    seconds.innerHTML = "00";
+    minutes.innerHTML = "00";
+
+    starting_time_mills = Date.now();
+}
